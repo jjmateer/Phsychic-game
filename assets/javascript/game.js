b7f28e0fe87583fc33f150d2092caa45d4efb3f0
John Mateer <jj.mateer@live.com>
2019-06-23 23:29:51 -0700

    javascript looks cleaner, made equation for correct guess=wins++, else guesses--, if out of guesses losses++, if losses=2 you lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,50 @@
+//Variables
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-
 var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("userchoice-text");
-var guessesLeft = document.getElementById("guessesLeft-text")
-var guessesSoFar = document.getElementById("guessesFar-text")
+var userchoiceText = document.getElementById("userchoice-text");
+var userGuessText = document.getElementById("userGuess-text");
+var guessesLeftText = document.getElementById("guessesLeft-text");
+var guessesSoFarText = document.getElementById("guessesFar-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
-
+//Choices for computer
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
     "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-
-document.onkeyup = function (event) {
-
-
-    var userChoice = event.key;
-
-
+////The computer picks a random letter from A-Z
     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+winsText.textContent = "wins: " + wins;
+lossesText.textContent = "losses: " + losses;
+guessesLeft.textContent = "guesses left:" + guessesLeft;
+//If the user and computer guess the same, "wins" +1, otherwise "guesses left" -1
+
+document.onkeyup = event => {
+    var userChoice = event.key.toLowerCase();
+    if(userChoice === computerGuess) {
+        wins++;
+        guessesLeft = 8;
+
+    }
+    else {
+        guessesLeft--;
+      }
+      
+      if (guessesLeft === 0){
+        losses++;
+        guessesLeft = 9;
+      }
+  
+    if(losses === 1)
+      alert("you lose!")
 
     alert("User guess: " + userChoice);
 
-    userChoiceText.textContent = "You chose: " + userChoice;
+  
 
 
-    winsText.textContent = "wins: " + wins;
-    lossesText.textContent = "losses: " + losses;
 
-    guessesLeft.textContent = "guesses left:" + (guessesLeft - 1);
 
-    
-
-    if (userChoice === computerGuess) {
-    console.log(wins++)
-    }
 
 };
