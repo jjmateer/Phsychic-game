44b21bcd1fdd265b9aa89dcf327bda5c808a48a2
John Mateer <jj.mateer@live.com>
2019-06-24 18:22:01 -0700

    Wins, losses, and guesses left are fully functioning and now display on the website. Converted wins,losses ,and guesses left equations into a update text function. added update text tags when needed.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,41 +11,42 @@ var lossesText = document.getElementById("losses-text");
 //Choices for computer
 var computerChoices = "abcdefghijklmnopqrstuvwxyz".split("");
 ////The computer picks a random letter from A-Z
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
-winsText.textContent = "wins: " + wins;
-lossesText.textContent = "losses: " + losses;
-guessesLeft.textContent = "guesses left:" + guessesLeft;
+var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+function updateText() {
+  winsText.textContent = "wins: " + wins;
+  lossesText.textContent = "losses: " + losses;
+  guessesLeftText.textContent = "guesses left:" + guessesLeft;
+}
 //If the user and computer guess the same, "wins" +1, otherwise "guesses left" -1
+updateText();
 
 document.onkeyup = event => {
-    var userGuess = event.key.toLowerCase();
-    if(userGuess === computerGuess) {
-        wins++;
-        guessesLeft = 8;
+  var userGuess = event.key.toLowerCase();
+  if (userGuess === computerGuess) {
+    wins++;
+    updateText();
+    guessesLeft = 8;
 
-    }
-    else {
-        guessesLeft--;
-      }
-      
-      if (guessesLeft === 0){
-        losses++;
-        guessesLeft = 9;
-      }
-  
-    if(losses === 1){
-      alert("you lose!")
-    }
-    if(wins ===1){
-      alert("you win!")
-    }
-    
-    alert("User guess: " + userGuess);
+  }
+  else {
+    guessesLeft--;
+    updateText();
+  }
 
-  
+  if (guessesLeft === 0) {
+    losses++;
+    updateText();
+    guessesLeft = 9;
+  }
 
+  if (losses === 1) {
+    alert("you lose!")
+  }
+  if (wins === 1) {
+    alert("you win!")
+  }
 
+  alert("User guess: " + userGuess);
 
 
 
