ed293d7681cc6ef60673ac3f008de94cc9d544be
John Mateer <jj.mateer@live.com>
2019-06-26 01:54:55 -0700

    added an onkeydown function that displays key that you press, trying to use it to list out guesses so far. Currently, the letter you chose dissapears when you close the alert.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,14 @@
 //Variables
 var wins = 0;
 var losses = 0;
-var guessesLeft = 9;
+var guessesLeft = 5;
 var directionsText = document.getElementById("directions-text");
 var userGuessText = document.getElementById("userGuess-text");
+var guessesSoFarText = document.getElementById("guessesSoFar-text")
 var guessesLeftText = document.getElementById("guessesLeft-text");
-var guessesSoFarText = document.getElementById("guessesFar-text");
 var winsText = document.getElementById("wins-text");
 var lossesText = document.getElementById("losses-text");
+
 //Choices for computer
 var computerChoices = "abcdefghijklmnopqrstuvwxyz".split("");
 ////The computer picks a random letter from A-Z
@@ -15,17 +16,28 @@ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.l
 function updateText() {
   winsText.textContent = "wins: " + wins;
   lossesText.textContent = "losses: " + losses;
-  guessesLeftText.textContent = "guesses left:" + guessesLeft;
+  guessesLeftText.textContent = "guesses left: " + guessesLeft;
+  guessesSoFarText.textContent = "guesses so far: " + guessesSoFarText;
 }
 //If the user and computer guess the same, "wins" +1, otherwise "guesses left" -1
 updateText();
 
+ document.onkeydown = function(event) {
+  console.log()
+  var key_press = String.fromCharCode(event.keyCode);
+   document.getElementById('guessesSoFar-text') .innerHTML = key_press;
+
+ }
+
 document.onkeyup = event => {
+  console.log()
+
   var userGuess = event.key.toLowerCase();
   if (userGuess === computerGuess) {
     wins++;
     updateText();
-    guessesLeft = 8;
+    guessesLeft = 4;
+    
 
   }
   else {
@@ -36,14 +48,18 @@ document.onkeyup = event => {
   if (guessesLeft === 0) {
     losses++;
     updateText();
-    guessesLeft = 9;
+    guessesLeft = 5;
   }
 
-  if (losses === 1) {
+  if (losses === 2) {
     alert("you lose!")
+    wins = 0;
+    losses = 0;
   }
   if (wins === 1) {
     alert("you win!")
+    wins = 0;
+    losses = 0;
   }
 
   alert("User guess: " + userGuess);
