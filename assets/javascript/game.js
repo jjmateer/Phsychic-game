ddc7458913a6712e2490178e03724492329eaea8
John Mateer <jj.mateer@live.com>
2019-06-27 12:55:05 -0700

    formatted document and took out uneccesary spaces, added text labels to html.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ var alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
 var wins = 0;
 var losses = 0;
 var guesses = 5;
-var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
+computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 var guessesSoFar = [];
 var winPoints = document.getElementById("wins-text");
 var guessesLeft = document.getElementById("guessesLeft-text");
@@ -15,34 +15,35 @@ function updateDisplays() {
   losePoints.textContent = losses;
 }
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
   var userGuess = event.key;
   guessesSoFar.push(userGuess);
   guessSoFar.textContent = guessesSoFar;
 
   if (userGuess == computerGuess) {
-
     wins++;
     guesses = 5
-    computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
     guessesSoFar = [];
     alert("+1 win")
-  } else {
 
+  } else {
     guesses--;
-    if(losses == 2){
+
+    if (losses == 2) {
       alert("you lose!");
       wins = 0;
       losses = 0;
     }
+
     if (guesses == 0) {
       losses++;
       guessesSoFar = [];
       guesses = 5;
       alert("+1 loss")
     }
-    if(wins == 2) {
+
+    if (wins == 2) {
       alert("you win!")
       wins = 0;
       losses = 0;
